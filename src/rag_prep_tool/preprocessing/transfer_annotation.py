f6dab1bd6393210012b5e3c55b9e402915276b0c
Tenzin <tenzintsunduebhattu@gmail.com>
2024-05-24 12:23:25 +0530

    func transfer page ann

---
+++ src/rag_prep_tool/preprocessing/transfer_annotation.py
@@ -0,0 +1,17 @@
+from fast_antx.core import transfer
+from pathlib import Path 
+
+
+def transfer_page_ann(source_text:str, target_text:str)->str:
+    """ replacing page breaks (\n\n) with ⏎ in source_text and removing page breaks in target_text"""
+    source_text = source_text.replace("\n\n", "⏎")
+    target_text = target_text.replace("\n\n", "")
+
+    annotations = [["page_breaks", "(⏎)"]]
+    page_annoted_text = transfer(source_text, annotations, target_text, output="txt")
+
+    return page_annoted_text
+
+
+
+
