202c2abca60f589448c17030d802d2521d922c99
Tenzin <tenzintsunduebhattu@gmail.com>
2024-05-24 17:13:22 +0530

    modify/change pdf extractor to PyMuPDF"

--- src/rag_prep_tool/preprocessing/extract_text.py
+++ src/rag_prep_tool/preprocessing/extract_text.py
@@ -1,20 +1,23 @@
-from pypdf import PdfReader
+
+import fitz  
 from pathlib import Path
 from typing import Dict 
 
+
 def extract_text_from_pdf_file(pdf_file_path: Path) -> Dict[int, str]:
-    """Reads the content of a PDF file using pypdf."""
+    """Reads the content of a PDF file using PyMuPDF."""
     text = {}
 
     try:
-        with open(pdf_file_path, "rb") as pdf_file:
-            pdf_reader = PdfReader(pdf_file)
-            for no, page in enumerate(pdf_reader.pages):
-                text[no] = page.extract_text() if page.extract_text() else ""
+        pdf_document = fitz.open(pdf_file_path)
+        for no in range(len(pdf_document)):
+            page = pdf_document.load_page(no)
+            text[no] = page.get_text() if page.get_text() else ""
         return text
     except Exception as e:
-        print(f"pdf file {pdf_file_path} is corrupted")
+        print(f"Failed to read PDF file {pdf_file_path}: {e}")
         return {}
+
     
 
     
--- tests/preprocessing/extract_text/test_extract_text.py
+++ tests/preprocessing/extract_text/test_extract_text.py
@@ -8,7 +8,11 @@ def test_extract_text_from_pdf_file():
     DATA_DIR = Path(__file__).parent / "data"
     pdf_file_path = DATA_DIR / "The Art of Happiness at Work first page.pdf"
 
-    extracted_data = extract_text_from_pdf_file(pdf_file_path)
+    extracted_data = extract_text_from_pdf_file(pdf_file_path)    
+    
+
+
+
     with open(DATA_DIR / "expected_extracted_file.json", "r") as f:
         expected_extracted_data = json.load(f)
 
