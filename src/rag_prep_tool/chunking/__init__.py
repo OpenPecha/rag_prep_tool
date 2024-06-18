1a3a0108b16fd600acdc9a1a9590677f7e281fa4
Tenzin <tenzintsunduebhattu@gmail.com>
2024-06-18 10:08:15 +0530

    set default chunk overlap=100

--- src/rag_prep_tool/chunking/__init__.py
+++ src/rag_prep_tool/chunking/__init__.py
@@ -5,7 +5,7 @@ from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.schema import TextNode
 
-def chunk_files(file_paths: List[Path], chunk_size: int = 500, chunk_overlap: int = 50)->List[TextNode]:
+def chunk_files(file_paths: List[Path], chunk_size: int = 500, chunk_overlap: int = 100)->List[TextNode]:
     
     """ convert the files into structured nodes for the RAG model"""
     file_paths = [str(file_path) for file_path in file_paths]
@@ -15,7 +15,3 @@ def chunk_files(file_paths: List[Path], chunk_size: int = 500, chunk_overlap: in
 
     return chunks 
 
-if __name__ == "__main__":
-    file_paths = [Path("output/Ethics for the New Millennium_content.txt")]
-    chunks = chunk_files(file_paths)
-    print(chunks[0].text)
