c82c1cd21892cc329d18112006544d15446c748e
Tenzin <tenzintsunduebhattu@gmail.com>
2024-06-24 11:16:50 +0530

    extract page number from pdf file with start = 1

--- src/rag_prep_tool/preprocessing/extract_text.py
+++ src/rag_prep_tool/preprocessing/extract_text.py
@@ -12,7 +12,7 @@ def extract_text_from_pdf_file(pdf_file_path: Path) -> Dict[int, str]:
         pdf_document = fitz.open(pdf_file_path)
         for no in range(len(pdf_document)):
             page = pdf_document.load_page(no)
-            text[no] = page.get_text() if page.get_text() else ""
+            text[no+1] = page.get_text() if page.get_text() else ""
         return text
     except Exception as e:
         print(f"Failed to read PDF file {pdf_file_path}: {e}")
@@ -20,4 +20,3 @@ def extract_text_from_pdf_file(pdf_file_path: Path) -> Dict[int, str]:
 
     
 
-    
