5d80544c8352f4348836630f05d2bfb2d29ce37a
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-08-29 11:13:06 +0530

    coref text

---
+++ src/rag_prep_tool/graph_db/text_processing.py
@@ -0,0 +1,29 @@
+from src.rag_prep_tool.graph_db.get_chatgpt_response import get_chatgpt_response
+
+def coref_text(text:str):
+    """ Perform coreference resolution on text"""
+    prompt = f"""
+            ## Objective:
+            You are a top-tier algorithm designed for resolving coreferences in the input text to build a knowledge graph.
+
+            ## Instructions:
+            -Resolve all the coreferences in the following text.
+            -Replace the pronouns with the actual entities.
+            -Other than the resolved text, don't include any other information.
+            -The text content is book by Dalai Lama.So the pronoun 'I' refers to Dalai Lama.
+            
+            ## Output format:
+            Resolved text
+
+            [INPUT TEXT START]
+            {text}
+            [INPUT TEXT END]
+
+    """
+    
+    try:
+        response_text = "".join(get_chatgpt_response(prompt))
+        return response_text
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return ""
